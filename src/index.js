1660b9f9e2db4e5404f135e62492778990a84cd5
sergeytverdokhlebov <sergey.tverdokhlebov@gmail.com>
2020-10-25 21:34:02 +0200

    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+    function check(str, bracketsConfig) {
+        let config = bracketsConfig.flat();
+        let stack = [];
+
+        for (let bracket of str) {
+
+            let brIndex = config.indexOf(bracket);
+
+            if (brIndex === -1) {
+                continue;
+            }
+
+            if (config[brIndex] === config[stack[stack.length - 1]]) {
+                stack.pop();
+                continue;
+            }
+
+            if (brIndex % 2 === 0) {
+ 
+                stack.push(brIndex + 1);
+            } else {
+
+                if (stack.pop() !== brIndex) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
